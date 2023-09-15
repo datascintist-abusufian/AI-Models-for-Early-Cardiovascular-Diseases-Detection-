626c855c5f059275848254a4ab23b9c8affbbbec
Data Analyst <abusufian.tex.cu@gmail.com>
2023-09-15 23:54:25 +0100

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -2,54 +2,44 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-model_filename = 'model.pkl'
+def load_model():
+    model_filename = 'model.pkl'
+    with open(model_filename, 'rb') as file:
+        loaded_model = pickle.load(file)
+    return loaded_model
 
-with open(model_filename, 'rb') as file:
-    model = pickle.load(file)
-
-print("hello")
 def main():
-    # Load and display the logo
+    model = load_model()
+
+    # Display logo and title
     logo = "logo Heart.jpeg"
     st.image(logo, use_column_width=True, caption="Your Company Logo")
-
     st.title('AI-Models-for-Early-Cardiovascular-Diseases-Prediction')
     st.write("""### We need patient data to predict on""")
 
-# Call the main function
-if __name__ == "__main__":
-    main()
-
+    # Get input from user
     age = st.slider('Age', 18, 100, 50)
-    sex_options = ['Male', 'Female']
-    sex = st.selectbox('Sex', sex_options)
+    sex = st.selectbox('Sex', ['Male', 'Female'])
     sex_num = 1 if sex == 'Male' else 0 
-    cp_options = ['Typical Angina', 'Atypical Angina', 'Non-anginal Pain', 'Asymptomatic']
-    cp = st.selectbox('Chest Pain Type', cp_options)
-    cp_num = cp_options.index(cp)
+    cp = st.selectbox('Chest Pain Type', ['Typical Angina', 'Atypical Angina', 'Non-anginal Pain', 'Asymptomatic'])
+    cp_num = cp.index(cp)
     trestbps = st.slider('Resting Blood Pressure', 90, 200, 120)
     chol = st.slider('Cholesterol', 100, 600, 250)
-    fbs_options = ['False', 'True']
-    fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', fbs_options)
-    fbs_num = fbs_options.index(fbs)
-    restecg_options = ['Normal', 'ST-T Abnormality', 'Left Ventricular Hypertrophy']
-    restecg = st.selectbox('Resting Electrocardiographic Results', restecg_options)
-    restecg_num = restecg_options.index(restecg)
+    fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', ['False', 'True'])
+    fbs_num = fbs.index(fbs)
+    restecg = st.selectbox('Resting Electrocardiographic Results', ['Normal', 'ST-T Abnormality', 'Left Ventricular Hypertrophy'])
+    restecg_num = restecg.index(restecg)
     thalach = st.slider('Maximum Heart Rate Achieved', 70, 220, 150)
-    exang_options = ['No', 'Yes']
-    exang = st.selectbox('Exercise Induced Angina', exang_options)
-    exang_num = exang_options.index(exang)
+    exang = st.selectbox('Exercise Induced Angina', ['No', 'Yes'])
+    exang_num = exang.index(exang)
     oldpeak = st.slider('ST Depression Induced by Exercise Relative to Rest', 0.0, 6.2, 1.0)
-    slope_options = ['Upsloping', 'Flat', 'Downsloping']
-    slope = st.selectbox('Slope of the Peak Exercise ST Segment', slope_options)
-    slope_num = slope_options.index(slope)
+    slope = st.selectbox('Slope of the Peak Exercise ST Segment', ['Upsloping', 'Flat', 'Downsloping'])
+    slope_num = slope.index(slope)
     ca = st.slider('Number of Major Vessels Colored by Fluoroscopy', 0, 4, 1)
-    thal_options = ['Normal', 'Fixed Defect', 'Reversible Defect']
-    thal = st.selectbox('Thalassemia', thal_options)
-    thal_num = thal_options.index(thal)
+    thal = st.selectbox('Thalassemia', ['Normal', 'Fixed Defect', 'Reversible Defect'])
+    thal_num = thal.index(thal)
 
-
-
+    # Predict when button is pressed
     if st.button('Predict'):
         user_input = pd.DataFrame(data={
             'age': [age],
@@ -66,6 +56,7 @@ if __name__ == "__main__":
             'ca': [ca],
             'thal': [thal_num]
         })
+        
         prediction = model.predict(user_input)
         prediction_proba = model.predict_proba(user_input)
 
@@ -77,7 +68,6 @@ if __name__ == "__main__":
             prediction_result = 'Negative'
         
         confidence = prediction_proba[0][1] if prediction[0] == 1 else prediction_proba[0][0]
-
         st.markdown(f"<p style='background-color:{bg_color}; color:white; padding:10px;'>Prediction: {prediction_result}<br>Confidence: {((confidence*10000)//1)/100}%</p>", unsafe_allow_html=True)
 
 if __name__ == '__main__':
